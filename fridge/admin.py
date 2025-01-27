580dc49a882de347b414c706be5ab4e8f64bd9d9
ssam-176 <sgates61@outlook.com>
2025-01-27 10:48:40 +0000

    database stuff

--- fridge/admin.py
+++ fridge/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AmountType, ItemType, IndividualItem, ShoppingList
 
-# Register your models here.
+admin.site.register(AmountType)
+admin.site.register(ItemType)
+admin.site.register(IndividualItem)
+admin.site.register(ShoppingList)
--- fridge/models.py
+++ fridge/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class AmountType(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemType(models.Model):
+    unique_barcode = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=255)
+    amount_type = models.ForeignKey(AmountType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class IndividualItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    expiration_date = models.DateField()
+    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return f"{self.type.name} - {self.expiration_date}"
+
+
+class ShoppingList(models.Model):
+    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+    class Meta:
+        unique_together = ('item_type', 'amount')  # Composite primary key
+
+    def __str__(self):
+        return f"Shopping List Item: {self.item_type.name} - Amount: {self.amount}"
